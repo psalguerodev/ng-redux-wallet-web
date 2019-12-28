20422ed0b616b202df03bd1ddaa7436886e649cd
Patrick Salguero <patrick.salguero.avalos@gmail.com>
2019-12-27 21:29:40 -0500

    was(firebase): Was configured firebase hosting

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,16 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AuthService } from '../../auth/services/auth.service';
+import { User } from '../../auth/model/user.model';
+import { Subscription } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/app.reducer';
 
 @Component({
   selector: 'psalguerodev-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent implements OnInit, OnDestroy {
 
-  constructor(private authService: AuthService) { }
+  user: User;
+  userSubscription: Subscription;
+
+  constructor(
+    private store: Store<AppState>,
+    private authService: AuthService) { }
 
   ngOnInit() {
+    this.userSubscription = this.store.select('auth')
+      .subscribe(userState => this.user = userState.user);
+  }
+
+  ngOnDestroy() {
+    if (this.userSubscription) this.userSubscription.unsubscribe();
   }
 
   logout() {
